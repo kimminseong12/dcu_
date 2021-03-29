db3d1d64094890aad8b6305e7010c6a71984aa6f
Kimminseong <okok9018@cu.ac.kr>
2021-03-29 14:45:51 +0900

    .

---
+++ 예제 3/예제 3-1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void manin (){
+	int i, sale[4] = { 157, 209, 251, 312 };
+
+	for (i = 0; i < 4; i++) {
+		printf("\n address : %u sale[%d]= %d", &sale[i], i, sale[i]);
+	}
+	getchar();
+}
